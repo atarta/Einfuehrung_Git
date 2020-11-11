f6bbf2a04eebedc9c7f21cefa0414fedd30a079b
atarta <atarta@htl-steyr.ac.at>
2020-11-11 11:47:49 +0100

    Änderung Konflikt wird erzwungen

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hallo Stefi");
         System.out.println("Hallo Andrea");
+
+
+        System.out.println("I like 187");
     }
 }
