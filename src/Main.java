63d6c072086f521059b391d39c44e1c56bbe4cd5
saichhor <saichhor@htl-steyr.ac.at>
2020-11-11 11:46:31 +0100

    Änderungen: Conflict wird hinzugefügt.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hallo Stefi");
+
         System.out.println("Hallo Andrea");
+
+        System.out.println("Hallo Sugar Daddy!");
     }
 }
